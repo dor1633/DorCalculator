0eb4ddcaf6aa8f89aaa090a79ab184c375852312
dor1633 <dor1633@gmail.com>
2020-03-18 10:05:45 +0200

    update ui

--- main.py
+++ main.py
@@ -1,146 +1,62 @@
-# Python program to  create a simple GUI  
-# calculator using Tkinter 
-
-# import everything from tkinter module 
 from tkinter import *
 
-# globally declare the expression variable 
-expression = ""
+# look at https://www.simplifiedpython.net/python-calculator/
+
+def iCalc(source, side):
+    storeObj = Frame(source, borderwidth=4, bd=4, bg="powder blue")
+    storeObj.pack(side=side, expand =YES, fill =BOTH)
+    return storeObj
+
+def button(source, side, text, command=None):
+    storeObj = Button(source, text=text, command=command)
+    storeObj.pack(side=side, expand = YES, fill=BOTH)
+    return storeObj
+
+class app(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+        self.option_add('*Font', 'arial 20 bold')
+        self.pack(expand = YES, fill =BOTH)
+        self.master.title('Calculator')
+
+        display = StringVar()
+        Entry(self, relief=RIDGE, textvariable=display,
+          justify='right'
+          , bd=30, bg="powder blue").pack(side=TOP,
+                                          expand=YES, fill=BOTH)
+
+        for clearButton in (["C"]):
+            erase = iCalc(self, TOP)
+            for ichar in clearButton:
+                button(erase, LEFT, ichar, lambda
+                    storeObj=display, q=ichar: storeObj.set(''))
+
+        for numButton in ("789/", "456*", "123-", "0.+"):
+         FunctionNum = iCalc(self, TOP)
+         for iEquals in numButton:
+            button(FunctionNum, LEFT, iEquals, lambda
+                storeObj=display, q=iEquals: storeObj
+                   .set(storeObj.get() + q))
+
+        EqualButton = iCalc(self, TOP)
+        for iEquals in "=":
+            if iEquals == '=':
+                btniEquals = button(EqualButton, LEFT, iEquals)
+                btniEquals.bind('<ButtonRelease-1>', lambda e,s=self,
+                                storeObj=display: s.calc(storeObj), '+')
 
 
-# Function to update expressiom 
-# in the text entry box 
-def press(num):
-    # point out the global expression variable 
-    global expression
+            else:
+                btniEquals = button(EqualButton, LEFT, iEquals,
+                                    lambda storeObj=display, s=' %s ' % iEquals: storeObj.set
+                                    (storeObj.get() + s))
 
-    # concatenation of string 
-    expression = expression + str(num)
-
-    # update the expression by using set method 
-    equation.set(expression)
-
-
-# Function to evaluate the final expression 
-def equalpress():
-    # Try and except statement is used 
-    # for handling the errors like zero 
-    # division error etc. 
-
-    # Put that code inside the try block 
-    # which may generate the error 
-    try:
-
-        global expression
-
-        # eval function evaluate the expression 
-        # and str function convert the result 
-        # into string 
-        total = str(eval(expression))
-
-        equation.set(total)
-
-        # initialze the expression variable 
-        # by empty string 
-        expression = ""
-
-        # if error is generate then handle
-    # by the except block 
-    except:
-
-        equation.set(" error ")
-        expression = ""
-
-    # Function to clear the contents
+    def calc(self, display):
+            try:
+                display.set(eval(display.get()))
+            except:
+                display.set("ERROR")
 
 
-# of text entry box
-def clear():
-    global expression
-    expression = ""
-    equation.set("")
-
-
-# Driver code 
-if __name__ == "__main__":
-    # create a GUI window 
-    gui = Tk()
-
-    # set the background colour of GUI window 
-    gui.configure(background="light green")
-
-    # set the title of GUI window 
-    gui.title("Simple Calculator")
-
-    # set the configuration of GUI window 
-    gui.geometry("265x125")
-
-    # StringVar() is the variable class 
-    # we create an instance of this class 
-    equation = StringVar()
-
-    # create the text entry box for 
-    # showing the expression . 
-    expression_field = Entry(gui, textvariable=equation)
-
-    # grid method is used for placing 
-    # the widgets at respective positions 
-    # in table like structure . 
-    expression_field.grid(columnspan=4, ipadx=70)
-
-    equation.set('enter your expression')
-
-    # create a Buttons and place at a particular 
-    # location inside the root window . 
-    # when user press the button, the command or 
-    # function affiliated to that button is executed . 
-    button1 = Button(gui, text=' 1 ', fg='black', bg='red',
-                     command=lambda: press(1), height=1, width=7)
-    button1.grid(row=2, column=0)
-
-    button2 = Button(gui, text=' 2 ', fg='black', bg='red',
-                     command=lambda: press(2), height=1, width=7)
-    button2.grid(row=2, column=1)
-
-    button3 = Button(gui, text=' 3 ', fg='black', bg='red',
-                     command=lambda: press(3), height=1, width=7)
-    button3.grid(row=2, column=2)
-
-    button4 = Button(gui, text=' 4 ', fg='black', bg='red',
-                     command=lambda: press(4), height=1, width=7)
-    button4.grid(row=3, column=0)
-
-    button5 = Button(gui, text=' 5 ', fg='black', bg='red',
-                     command=lambda: press(5), height=1, width=7)
-    button5.grid(row=3, column=1)
-
-    button6 = Button(gui, text=' 6 ', fg='black', bg='red',
-                     command=lambda: press(6), height=1, width=7)
-    button6.grid(row=3, column=2)
-
-    button7 = Button(gui, text=' 7 ', fg='black', bg='red',
-                     command=lambda: press(7), height=1, width=7)
-    button7.grid(row=4, column=0)
-
-    button8 = Button(gui, text=' 8 ', fg='black', bg='red',
-                     command=lambda: press(8), height=1, width=7)
-    button8.grid(row=4, column=1)
-
-    button9 = Button(gui, text=' 9 ', fg='black', bg='red',
-                     command=lambda: press(9), height=1, width=7)
-    button9.grid(row=4, column=2)
-
-    button0 = Button(gui, text=' 0 ', fg='black', bg='red',
-                     command=lambda: press(0), height=1, width=7)
-    button0.grid(row=5, column=0)
-
-    equal = Button(gui, text=' = ', fg='black', bg='red',
-                   command=equalpress, height=1, width=7)
-    equal.grid(row=5, column=2)
-
-    clear = Button(gui, text='Clear', fg='black', bg='red',
-                   command=clear, height=1, width=7)
-    clear.grid(row=5, column='1')
-
-    # start the GUI 
-    gui.mainloop()
+if __name__=='__main__':
+ app().mainloop()
